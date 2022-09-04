450e1e6dc20a11f4f0b2981225e7435ced8fe151
anandakumarpalanisamy <ananda11k@gmail.com>
2022-09-04 23:43:20 +0100

    Demonstrate Grid Options

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 import "ag-grid-community/dist/styles/ag-theme-alpine-dark.css";
 
 function App() {
-  const [rowData, setRowData] = useState([]);
+  const [rowData, setRowData] = useState();
 
-  const gridOptions = {
-    rowSelection: "multiple",
-    animateRows: true,
-  };
+  const gridOptions = useMemo(
+    () => ({
+      rowSelection: "multiple",
+      animateRows: true,
+    }),
+    []
+  );
 
-  const columnDefs = [
+  const [columnDefs] = useState([
     { field: "athlete" },
     { field: "age" },
     { field: "country" },
@@ -24,12 +27,15 @@ function App() {
     { field: "silver" },
     { field: "bronze" },
     { field: "total" },
-  ];
+  ]);
 
-  const defaultColDef = {
-    sortable: true,
-    filter: true,
-  };
+  const defaultColDef = useMemo(
+    () => ({
+      sortable: true,
+      filter: true,
+    }),
+    []
+  );
 
   useEffect(() => {
     fetch("https://www.ag-grid.com/example-assets/olympic-winners.json")
